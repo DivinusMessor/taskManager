0331e3cf09795947bafad91a7aa56dd95f74c47f
Yukio <DivinusMessor@users.noreply.github.com>
2023-04-19 18:52:16 -0700

    added a few changes, SS removed and added metadata to html

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "tasks.db")
 
 # Disable the tracking of modifications to save resources 
-SSQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
