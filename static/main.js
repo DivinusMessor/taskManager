b9d708a91cf79f099a98e04fdb77461cb2c775b8
Yukio <DivinusMessor@users.noreply.github.com>
2023-04-21 11:29:44 -0600

    moved js to main.js and out of index.html

--- static/main.js
+++ static/main.js
@@ -1,4 +1,6 @@
 function deleteTask(taskId) {
+  const taskCard = document.getElementById("task-card-" + taskId);
+
   // Call your delete task API
   fetch(`/tasks/${taskId}`, { // Change /delete_task/ to /tasks/
   method: 'DELETE',
@@ -51,3 +53,185 @@ document.querySelectorAll(".complete-task").forEach((button) => {
     toggleTaskCompletion(taskId, completed);
   });
 });
+
+function toggleTodo(todo_id) {
+  const checkbox = document.getElementById("checkbox-" + todo_id);
+  const content = document.getElementById("todo-content-" + todo_id);
+
+  if (checkbox.checked) {
+    content.style.textDecoration = "line-through";
+  } else {
+    content.style.textDecoration = "none";
+  }
+
+  // You can also call an API here to update the todo.completed status on the server-side
+}
+
+// Modify the editTask function
+function editTask(task_id) {
+  const taskTitle = document.getElementById("task-title-" + task_id);
+  const todoItems = document.querySelectorAll(
+    "#task-card-" + task_id + " .list-group-item"
+  );
+  const editButton = document.getElementById("edit-task-" + task_id);
+  const isEditable = taskTitle.getAttribute("contenteditable") === "true";
+  const taskCard = document.getElementById("task-card-" + task_id);
+  const taskDescription = taskCard.querySelector(".card-text");
+  const addTodoButton = taskCard.querySelector(".add-todo-button");
+
+  if (isEditable) {
+    taskTitle.setAttribute("contenteditable", "false");
+    editButton.textContent = "Edit";
+
+    todoItems.forEach((todoItem) => {
+      const todoId = todoItem.id.split("-")[2];
+      const todoContent = document.getElementById(
+        "todo-content-" + todoId
+      );
+      const removeTodoButton = todoItem.querySelector(".remove-todo");
+      const moveUpTodoButton = todoItem.querySelector(".move-up-todo");
+      const moveDownTodoButton =
+        todoItem.querySelector(".move-down-todo");
+      if (todoContent) {
+        todoContent.setAttribute("contenteditable", "false");
+      }
+      if (removeTodoButton) {
+        removeTodoButton.style.display = "none";
+      }
+      if (moveUpTodoButton) {
+        moveUpTodoButton.style.display = "none";
+      }
+      if (moveDownTodoButton) {
+        moveDownTodoButton.style.display = "none";
+      }
+    });
+
+    if (taskDescription) {
+      taskDescription.setAttribute("contenteditable", "false");
+    }
+    if (addTodoButton) {
+      addTodoButton.style.display = "none";
+    }
+
+    // Save changes to the server, if needed
+    // Update the task creation date to the current date
+  } else {
+    taskTitle.setAttribute("contenteditable", "true");
+    taskTitle.focus();
+    editButton.textContent = "Save";
+
+    todoItems.forEach((todoItem) => {
+      const todoId = todoItem.id.split("-")[2];
+      const todoContent = document.getElementById(
+        "todo-content-" + todoId
+      );
+      const removeTodoButton = todoItem.querySelector(".remove-todo");
+      const moveUpTodoButton = todoItem.querySelector(".move-up-todo");
+      const moveDownTodoButton =
+        todoItem.querySelector(".move-down-todo");
+      if (todoContent) {
+        todoContent.setAttribute("contenteditable", "true");
+      }
+      if (removeTodoButton) {
+        removeTodoButton.style.display = "inline";
+      }
+      if (moveUpTodoButton) {
+        moveUpTodoButton.style.display = "inline";
+      }
+      if (moveDownTodoButton) {
+        moveDownTodoButton.style.display = "inline";
+      }
+      if (taskDescription) {
+        taskDescription.setAttribute("contenteditable", "true");
+      }
+      if (addTodoButton) {
+        addTodoButton.style.display = "inline";
+      }
+    });
+  }
+}
+
+// Add this to your JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  const editTodoButtons = document.querySelectorAll(".edit-todo");
+
+  editTodoButtons.forEach((editTodoButton) => {
+    editTodoButton.style.display = "none";
+  });
+});
+
+function editTodo(todo_id) {
+  const todoContent = document.getElementById("todo-content-" + todo_id);
+  const editTodoButton = document.querySelector(
+    `[data-todo-id="${todo_id}"]`
+  );
+  const pencilIcon = editTodoButton.querySelector(".fas.fa-pencil-alt");
+  const checkIcon = editTodoButton.querySelector(".fas.fa-check");
+
+  if (todoContent.getAttribute("contenteditable") !== "true") {
+    todoContent.setAttribute("contenteditable", "true");
+    todoContent.focus();
+    pencilIcon.style.display = "none";
+    checkIcon.style.display = "inline-block";
+  } else {
+    todoContent.removeAttribute("contenteditable");
+    pencilIcon.style.display = "inline-block";
+    checkIcon.style.display = "none";
+    // Call your update todo API to save the changes
+  }
+}
+
+function removeTodo(todo_id) {
+  const todoItem = document.getElementById("todo-item-" + todo_id);
+  todoItem.remove();
+  // Call your delete todo API to remove the todo from the server-side
+}
+
+function moveTodoUp(todo_id) {
+  const todoItem = document.getElementById("todo-item-" + todo_id);
+  const previousItem = todoItem.previousElementSibling;
+  if (previousItem) {
+    todoItem.parentNode.insertBefore(todoItem, previousItem);
+    // Call your update todo API to update the order of todos on the server-side
+  }
+}
+
+function moveTodoDown(todo_id) {
+  const todoItem = document.getElementById("todo-item-" + todo_id);
+  const nextItem = todoItem.nextElementSibling;
+  if (nextItem) {
+    todoItem.parentNode.insertBefore(nextItem, todoItem);
+    // Call your update todo API to update the order of todos on the server-side
+  }
+}
+
+function addTodo(task_id) {
+  const taskCard = document.getElementById("task-card-" + task_id);
+  const todosList = taskCard.querySelector(".list-group");
+
+  const newTodoItem = document.createElement("li");
+  newTodoItem.className = "list-group-item";
+  newTodoItem.innerHTML = `
+    <input type="checkbox">
+    <span contenteditable="true">New Todo</span>
+    <button class="btn btn-danger btn-sm remove-todo hide-button" onclick="removeTodo(this.parentElement)">Remove</button>
+    <button class="btn btn-secondary btn-sm move-up-todo hide-button" onclick="moveTodoUp(this.parentElement)">Up</button>
+    <button class="btn btn-secondary btn-sm move-down-todo hide-button" onclick="moveTodoDown(this.parentElement)">Down</button>
+  `;
+
+  todosList.appendChild(newTodoItem);
+}      
+
+function saveNewTodo(task_id, todoItem) {
+  const newTodoContent = todoItem.querySelector("span[contenteditable='true']");
+
+  if (newTodoContent.textContent.trim() === "") {
+    alert("Please enter some text for the new todo.");
+    return;
+  }
+
+  // Call your create todo API to add the new todo on the server-side
+  // Pass task_id and newTodoContent.textContent as parameters to your API
+  // On successful creation of the new todo, update the todoItem with the appropriate data and event listeners
+  // Example: Assign an ID, make the content non-editable, add the other buttons, etc.
+}
